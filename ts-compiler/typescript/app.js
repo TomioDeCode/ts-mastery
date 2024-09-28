660c24ad33695ce1b03db1fd8509e451df6878bb
TomioDeCode <tomiodecode@gmail.com>
2024-09-28 17:06:13 +0000

    docs: lib

--- ts-compiler/typescript/app.js
+++ ts-compiler/typescript/app.js
@@ -3,3 +3,10 @@
  * LECTURE 01: TYPESCRIPT || app.ts -w
  *****************************************/
 console.log("Typescript Compiler Configuration");
+/******************************************
+ * LECTURE 01: CORE || app.ts -w
+ *****************************************/
+const button = document.getElementById("btn");
+button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
+    console.log("Button is clicked!");
+});
--- ts-compiler/typescript/app.ts
+++ ts-compiler/typescript/app.ts
@@ -2,4 +2,14 @@
  * LECTURE 01: TYPESCRIPT || app.ts -w
  *****************************************/
 
-console.log("Typescript Compiler Configuration"); 
+console.log("Typescript Compiler Configuration");
+
+/******************************************
+ * LECTURE 01: CORE || app.ts -w
+ *****************************************/
+
+const button = document.getElementById("btn");
+
+button?.addEventListener("click", function () {
+  console.log("Button is clicked!");
+});
