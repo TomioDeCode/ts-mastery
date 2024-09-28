bd963ff526372fd84a446058b7f793de3aa30885
TomioDeCode <tomiodecode@gmail.com>
2024-09-28 17:53:51 +0000

    docs: destructuring object

--- ts-advance/dist/app.js
+++ ts-advance/dist/app.js
@@ -2,7 +2,18 @@
 /******************************************
  * LECTURE 01: ARRAY DESTRUCTURING
  *****************************************/
-const person = ["John", "Smith", 28, "Female"];
-const [firsName, lastName, age, gender = "Male"] = person;
-console.log(firsName, lastName, age, gender);
-console.log(person);
+// const person = ["John", "Smith", 28, "Female"];
+// const [firsName, lastName, age, gender = "Male"] = person;
+// console.log(firsName, lastName, age, gender);
+// console.log(person);
+/******************************************
+ * LECTURE 02: OBJECT DESTRUCTURING
+ *****************************************/
+const User = {
+    firsName: "John",
+    age: 27,
+    gender: "Male",
+    city: "Madiun",
+};
+const { firsName: fullName, age, gender, city } = User;
+console.log(fullName, age, gender, city);
--- ts-advance/src/ts/app.ts
+++ ts-advance/src/ts/app.ts
@@ -2,8 +2,22 @@
  * LECTURE 01: ARRAY DESTRUCTURING
  *****************************************/
 
-const person = ["John", "Smith", 28, "Female"];
+// const person = ["John", "Smith", 28, "Female"];
 
-const [firsName, lastName, age, gender = "Male"] = person;
-console.log(firsName, lastName, age, gender);
-console.log(person);
+// const [firsName, lastName, age, gender = "Male"] = person;
+// console.log(firsName, lastName, age, gender);
+// console.log(person);
+
+/******************************************
+ * LECTURE 02: OBJECT DESTRUCTURING
+ *****************************************/
+
+const User = {
+  firsName: "John",
+  age: 27,
+  gender: "Male",
+  city: "Madiun",
+};
+
+const { firsName: fullName, age,gender, city } = User;
+console.log(fullName, age, gender, city);
